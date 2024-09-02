40e772286878c338ee77e34e9efaf3d0471bd809
RyosukeSomeya <rx.0.0618@gmail.com>
2024-09-02 21:52:18 +0900

    タスク追加ページ作成

---
+++ src/components/TaskCard/TaskDeleteButton/TaskDeleteButton.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import { FaPen, FaTrashAlt } from 'react-icons/fa'
+
+interface TaskDeleteButtonProps {
+  id: string
+}
+const TaskDeleteButton = ({ id }: TaskDeleteButtonProps) => {
+  return (
+    <form action={''}>
+      <button type='submit' className='hover:text-gray-700 text-lg cursor-pointer'>
+        <FaTrashAlt />
+      </button>
+    </form>
+  )
+}
+
+export default TaskDeleteButton
